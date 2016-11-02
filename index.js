8317e07eafde6c19f19a424412d6e16a0e43701c
odriann <odriann23@gmail.com>
2016-11-02 16:13:37 +0000

    third verison nitrous code

---
+++ index.js
@@ -0,0 +1,26 @@
+// write your code below!
+
+function basicTeenager(age) {
+if(age>= 13 && age <=19){
+  //do something
+  return"You are a teenager!"
+}else{
+  // do something else
+ }
+}
+
+function teenager(age) {
+
+}
+
+function ageChecker(age) {
+
+}
+
+function ternaryTeenager(age) {
+
+}
+
+function switchAge(age) {
+
+}
